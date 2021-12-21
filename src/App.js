6a15e7290f5f52113cb6b57958d871057a61d23b
Your Name <you@example.com>
2021-12-21 09:42:53 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import React from 'react';
 
 class App extends React.Component{
   
-state={ todos: [{id:0,task:'eat', done:false},{id:1,task:'sleep', done:false},{id:1,task:'repeat', done:false}] ,text:""}
+state={ todos: [{id:0,task:'manger', done:false},{id:1,task:'dormir', done:false},{id:2,task:'révision', done:false}] ,text:""}
 
-ajouter=()=>{this.setState({  todos: [...this.state.todos, {id: Math.random(), done: false , task : this.state.text} ] })}
+ajouter=()=>{this.setState({  todos: [...this.state.todos, {id: Math.random(), task : this.state.text, done: false} ] })}
 supprimer =(id)=>{this.setState({todos: this.state.todos.filter(el=> id!== el.id)})}
-afficher = (id) =>{ this.setState({todos: this.state.todos.map ( el=>el===id ? {...el, done:! el.id} : el)})}
+afficher = (id) =>{ this.setState({todos: this.state.todos.map ( el=>el.id==id ? {...el, done:! el.done} : el)})}
 render() {
 console.log (this.state.todos)
    return(
 <div>
+<h1>My todo list</h1>
  <input onChange={(event)=>this.setState({text: event.target.value})}/>
 <buton onClick={()=> this.ajouter() }>Ajouter</buton>
-{this.state.todos.map (el =>
-<div>
-   <li> {el.task} </li>
-<buton  onClick={()=>this.supprimer(el.id)}> Supprimer</buton>
-<buton on onClick={()=>this.afficher(el.id)}>afficher</buton> 
+{this.state.todos.map (el => <div> <li style={{textDecoration:el.done?"line-through":null}}> {el.task} </li>
+
+<buton className='aff' onClick={()=>this.afficher(el.id)}>afficher</buton> 
+<buton className='sup' onClick={()=>this.supprimer(el.id)}> Supprimer</buton>
 </div>
 )}
 </div>
